d0f2d377c5674bddfc8154ed0f3eb26d799c06a7
Abhyudaya Avasthi <abhyudayaavasthi@Abhyudayas-MacBook-Air.local>
2021-10-17 18:51:11 +0530

    added static get nums function

--- game.py
+++ game.py
@@ -22,10 +22,31 @@ class logic:
             else:
                 self.grid[pos] = 2
 
+    @staticmethod
+    def _get_nums(this):
+        this_n = this[this != 0]
+        this_n_sum = []
+        skip = False
+
+        for j in range(len(this_n)):
+            if skip:
+                skip = False
+                continue
+            if j != len(this_n) - 1 and this_n[j] == this_n[j+1]:
+                new_n = this_n[j] * 2
+                skip = True
+
+            else:
+                new_n = this_n[j]
+
+            this_n_sum.append(new_n)
+
+        return np.array(this_n_sum)
+
     def movemade(self, move):
         for i in range(N):
             this = self.grid[i, :]  # all columns for this row
-            this_n = this[this != 0]
+            this_n = self._get_nums(this)
 
             new_this = np.zeros_like(this)
             new_this[:len(this_n)] = this_n
@@ -34,6 +55,7 @@ class logic:
 
 if __name__ == '__main__':
     game = logic()
+    game.newnumber(k=2)
     game.newnumber(k=2)
     print(game)
     game.movemade(move='l')
